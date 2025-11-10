ae3c0edc0acb7194bfe92d1719c57b3de29d26e1
hazem <hazem.hox@gmail.com>
2025-11-10 07:15:25 +0200

    More Code

---
+++ secrets-external-api/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import axios from 'axios';
+
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+
+
+
+// When the user goes to the home page it will render the index.ejs
+app.get("/", async (req, res) => {
+    try {
+        // Get the data form external api
+        const result = await axios.get("https://secrets-api.appbrewery.com/random");
+        res.render("index.ejs", {
+            secret: result.data.secret,
+            user: result.data.username,
+        });
+    } catch (error) {
+        console.log(error.response.data);
+        res.status(500);
+    }
+});
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
